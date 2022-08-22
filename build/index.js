7001591aa2024d413152a0a054944b9ca02c1a16
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-08-22 22:10:45 +0530

    chore: :tada: project initialized

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-const _dotenv = require("dotenv");
-const _app = require("./app");
-(0, _dotenv.config)();
-(0, _app.startServer)();
